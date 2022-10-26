2d10de1630d91cf7035d73327c425156a83bff14
Eprst-p <easter_p@mail.ru>
2022-10-26 16:40:17 +0600

    js без модуля

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log('Im here');
+
+const submitBtn = document.querySelector('.code-text-btn');
+
+const onSubmitBtnClick = () => {
+    alert('Oh my GODNESS');
+}
+
+submitBtn.addEventListener('click', onSubmitBtnClick);
