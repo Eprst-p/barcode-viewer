7dac30254253c5cb3bd69a00dda272bd00aeda9f
Eprst-p <easter_p@mail.ru>
2022-10-27 13:36:05 +0600

    папочки

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: false
+  },
+  module: {
+    rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        }
+    ]
+  }
+};
